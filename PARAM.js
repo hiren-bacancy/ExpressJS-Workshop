64a7a49ce1022ca8b5ad16ba51c079e79e602be6
hirenpanchal98 <hiren.panchal@bacancytechnology.com>
2019-01-11 13:43:13 +0530

    sixth

---
+++ PARAM.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+app.put('/message/:id',(req,res)=>{
+	const id = req.params.id;
+	const str = require('crypto').createHash('sha1')
+							.update(new Date().toDateString() + id)
+							.digest('hex');
+	res.send(str);
+}).listen(process.argv[2]);
