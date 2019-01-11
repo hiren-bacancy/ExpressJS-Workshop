6b7a3028226ae9292f6881f24c1664a57c3877e6
hirenpanchal98 <hiren.panchal@bacancytechnology.com>
2019-01-11 13:18:27 +0530

    fifth

---
+++ STYLIS-CSS.js
@@ -0,0 +1,4 @@
+const express =require('express');
+const app = express();
+app.use(require('stylus').middleware(process.argv[3]));
+app.use(express.static(process.argv[3])).listen(process.argv[2]);
